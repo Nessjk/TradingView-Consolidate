cde537687b92635bd12c4e77f8496fb88ebd9e61
tony <nessjk@gmail.com>
2022-04-02 10:58:30 +0200

    ADD - analytics gTag

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import App from "./App.vue";
 // CSS
 import "@/input.css";
 
+import VueGtag from "vue-gtag";
+
+createApp(App)
+  .use(VueGtag, {
+    config: { id: "G-GE514R4JPK" },
+  })
+  .mount("#app");
+
 Vue.config.productionTip = false;
 
 new Vue({
